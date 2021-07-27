60c6cdacec9210e2970efeee0d94360bb51efa7d
Denis Mythnik <themrcrovvley@gmail.com>
2021-07-27 16:27:21 +0300

    feat: add functions mix, max, avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (arr) {
+  return !arr ? 0 : !arr.length ? 0 : Math.min(...arr);
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (arr) {
+  return !arr ? 0 : !arr.length ? 0 : Math.max(...arr);
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (arr) {
+  if (!arr || !arr.length) {
+    return 0;
+  }
+  const res = arr.reduce((acc, cur) => {
+		acc += cur;
+		return acc;
+	}, 0);
+	return (res / (arr.length)).toFixed(2);
 }
